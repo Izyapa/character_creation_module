e655fb60a8aa7bdbd07f63cf65252fd86722a232
IslamAsaliev <asaliev.isl@yandex.ru>
2023-10-26 14:27:47 +0300

    Finish

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randint
 
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
-DEFAULT_STAMINA = 80 
+DEFAULT_STAMINA = 80
 
 
 class Character:
@@ -62,6 +62,44 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита' 
 
 
-warrior = Warrior('Кодослав')
-print(warrior)
-print(warrior.attack())
+def choice_char_class(char_name: str) -> Character:
+    """Возвращает строку с выбраннымклассом персонажа."""
+    # Добавили словарь, в котором соотносится ввод пользователя и класс персонажа.
+    game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
+    
+    approve_choice: str  = None
+    
+    while approve_choice != 'y':
+        selected_class = input('Введи название персонажа, '
+                           'за которого хочешь играть: Воитель — warrior, '
+                           'Маг — mage, Лекарь — healer: ')
+        char_class: Character = game_classes[selected_class](char_name)
+        # Вывели в терминал описание персонажа.
+        print(char_class)
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
+    return char_class
+
+
+def start_training(character) -> str:
+    """Начало тренировки."""
+    print('Потренируйся управлять своими навыками.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
+    print('Если не хочешь тренироваться, введи команду skip.')
+
+    dict_activ = {
+        'attack': character.attack,
+        'defence': character.defence,
+        'special': character.special
+    }
+    cmd: str = None
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        if cmd in dict_activ:
+            return (dict_activ[cmd])
+    return 'Тренировка окончена.'
+
+print(start_training(Warrior))
